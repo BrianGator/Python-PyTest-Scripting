a27d274fb4d1eff0b16c6295f956b0559fb16e01
Dave Westerveld <davewesterveld@gmail.com>
2025-08-21 18:56:29 -0400

    update scripts

--- course_scripts/02_06/02_06_final.py
+++ course_scripts/02_06/02_06_final.py
@@ -1,57 +1,56 @@
 from playwright.sync_api import Page, Playwright, sync_playwright
 
+url = "<your url here>"
+
 
 def open_gig_form(page: Page):
     page.get_by_role("button", name="Add a Gig").click()
 
 
 def add_venue(page: Page):
-    page.get_by_label("Venue").select_option("/venues")
-    page.goto("http://localhost:4001/venues")
+    page.goto(f"{url}/venues")
     page.get_by_role("button", name="Add a Venue").click()
-    page.get_by_placeholder("Enter the Venue name").click()
-    page.get_by_placeholder("Enter the Venue name").fill("Golf Course")
-    page.get_by_placeholder("Enter the Venue name").press("Tab")
-    page.get_by_label("Adress").fill("123 Main street")
-    page.get_by_label("Adress").press("Tab")
-    page.get_by_label("City", exact=True).fill("Somewhere")
-    page.get_by_label("City", exact=True).press("Tab")
-    page.get_by_label("Contact Number").fill("123-123-1234")
-    page.get_by_label("Contact Number").press("Tab")
-    page.get_by_label("Contact Email Address").fill("test@test.com")
-    page.get_by_label("Contact Email Address").press("Tab")
-    page.get_by_label("Venue Capacity").fill("100")
-    page.get_by_label("Venue Capacity").press("Tab")
-    page.get_by_label("Notes").fill("Cool place")
+    page.get_by_role("textbox", name="Name").click()
+    page.get_by_role("textbox", name="Name").fill("My Venue")
+    page.get_by_role("textbox", name="Name").press("Tab")
+    page.get_by_role("textbox", name="Adress").fill("123 Main street")
+    page.get_by_role("textbox", name="Adress").press("Tab")
+    page.get_by_role("textbox", name="Contact Number").fill("123-456-7658")
+    page.get_by_role("textbox", name="Contact Email Address").click()
+    page.get_by_role("textbox", name="Contact Email Address").fill("test@test.com")
+    page.get_by_role("spinbutton", name="Venue Capacity").click()
+    page.get_by_role("spinbutton", name="Venue Capacity").fill("100")
+    page.get_by_role("textbox", name="Notes").click()
+    page.get_by_role("textbox", name="Notes").fill("notes")
     page.get_by_role("button", name="Submit").click()
+    page.pause()
 
 
 def add_client(page: Page):
-    page.get_by_label("Client").select_option("/clients")
-    page.goto("http://localhost:4001/clients")
+    page.get_by_role("link", name=" Clients").click()
     page.get_by_role("button", name="Add a Client").click()
-    page.get_by_placeholder("Enter the Client's first name").click()
-    page.get_by_placeholder("Enter the Client's first name").fill("David")
-    page.get_by_placeholder("Enter the Client's first name").press("Tab")
-    page.get_by_placeholder("Enter the Client's last name").fill("Westerveld")
-    page.get_by_placeholder("Enter the Client's last name").press("Tab")
-    page.locator("#client-address").fill("987 Second Ave")
+    page.get_by_role("textbox", name="Enter the Client's first name").click()
+    page.get_by_role("textbox", name="Enter the Client's first name").fill("Dave")
+    page.get_by_role("textbox", name="Enter the Client's first name").press("Tab")
+    page.get_by_role("textbox", name="Enter the Client's last name").fill("Westerveld")
+    page.get_by_role("textbox", name="Enter the Client's last name").press("Tab")
+    page.locator("#client-address").fill("123 Main Street")
     page.locator("#client-address").press("Tab")
-    page.locator("#client-city").fill("Pleansantville")
+    page.locator("#client-city").fill("Somewhere")
     page.locator("#client-city").press("Tab")
-    page.locator("#client-contact_number").fill("987-987-0987")
+    page.locator("#client-contact_number").fill("123-432-4321")
     page.locator("#client-contact_number").press("Tab")
-    page.locator("#client-contact_email").fill("test2@test.com")
+    page.locator("#client-contact_email").fill("test@test.com")
     page.get_by_role("button", name="Submit").click()
 
 
 def add_gig(page: Page):
     page.get_by_role("button", name="Add a Gig").click()
-    page.get_by_label("Venue").select_option("1")
-    page.get_by_label("Client").select_option("1")
-    page.get_by_placeholder("Enter gig name").click()
-    page.get_by_placeholder("Enter gig name").fill("Gig Test")
-    page.get_by_label("Gig Date").fill("2025-01-30")
+    page.get_by_role("textbox", name="Gig Name").click()
+    page.get_by_role("textbox", name="Gig Name").fill("Test Gig")
+    page.get_by_role("textbox", name="Gig Date").fill("2025-08-21")
+    page.getByLabel("Venue").select_option("13")
+    page.getByLabel("Client").select_option("11")
     page.get_by_role("button", name="Submit").click()
 
 
@@ -59,21 +58,18 @@ def run(playwright: Playwright):
     firefox = playwright.firefox
     browser = firefox.launch(headless=False)
     page = browser.new_page()
-    url = "http://localhost:4001"
 
     page.goto(url)
+    page.get_by_role("button", name="Continue", exact=True).click()
 
     open_gig_form(page)
 
     # Scenario 1: Create a gig with a new client and a new venue
     add_venue(page)
 
-    page.get_by_role("link", name=" Home").click()
-    page.get_by_role("button", name="Add a Gig").click()
-
     add_client(page)
 
-    page.get_by_role("link", name=" Home").nth(1).click()
+    page.get_by_role("link", name=" Home").click()
 
     add_gig(page)
 
--- course_scripts/02_06/02_06_initial.py
+++ course_scripts/02_06/02_06_initial.py
@@ -1,5 +1,7 @@
 from playwright.sync_api import Page, Playwright, sync_playwright
 
+url = "<your url here>"
+
 
 def open_gig_form(page: Page):
     page.get_by_role("button", name="Add a Gig").click()
@@ -9,7 +11,6 @@ def run(playwright: Playwright):
     firefox = playwright.firefox
     browser = firefox.launch(headless=False)
     page = browser.new_page()
-    url = "http://localhost:4001"
 
     page.goto(url)
 
