98eb9001c76e92af5f8f18d49a860682ab72fd52
Dave Westerveld <davewesterveld@gmail.com>
2025-08-21 17:43:46 -0400

    additional cleanup

--- course_scripts/02_02/02_02.py
+++ course_scripts/02_02/02_02.py
@@ -1,12 +1,12 @@
 from playwright.sync_api import Playwright, sync_playwright
 
+url = "<your url here>"
+
 
 def run(playwright: Playwright):
     firefox = playwright.firefox
     browser = firefox.launch(headless=False)
     page = browser.new_page()
-    url = "http://localhost:4001"
-
     page.goto(url)
     page.get_by_role("button", name="Continue", exact=True).click()
     page.get_by_role("link", name=" Gigs").click()
